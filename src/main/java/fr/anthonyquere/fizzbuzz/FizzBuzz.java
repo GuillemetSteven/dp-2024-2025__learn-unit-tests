5e93b579a114948ea159875a8a40f3e6e9a4ef98
GuillemetSteven <berryfart2@gmail.com>
2025-03-11 10:52:08 +0100

    4/7 passed

--- src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,6 @@
 package fr.anthonyquere.fizzbuzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
@@ -9,6 +10,22 @@ public class FizzBuzz {
     }
 
     public static List<String> startFizzBuzz(int count) {
-        return null;
+
+        List<String> res = new ArrayList<>();
+
+        for (int i = 1; i <count; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 ==0) {
+                res.add("Buzz");
+            }
+            else {
+                res.add(Integer.toString(i));
+            }
+        }
+
+        return  res;
     }
 }
