8ca1c33a89633bb319bbb7a1586ee98079aa03f7
GuillemetSteven <berryfart2@gmail.com>
2025-03-12 08:43:47 +0100

    Create TeaShopTest

---
+++ src/test/java/fr/anthonyquere/TeaShop/TeaShopTest.java
@@ -0,0 +1,15 @@
+package fr.anthonyquere.TeaShop;
+
+import fr.anthonyquere.fizzbuzz.FizzBuzz;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TeaShopTest {
+
+    @Test
+    void should_() {
+
+    }
+}
